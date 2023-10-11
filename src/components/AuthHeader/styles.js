19a9afb6a9a601eabdde70cf2991d86d5ee7774c
John Doe <johndoe@email.com>
2023-10-11 14:31:44 +0300

    Input komponent

--- src/components/AuthHeader/styles.js
+++ src/components/AuthHeader/styles.js
@@ -6,6 +6,7 @@ export const styles = StyleSheet.create({
         padding: 24,
         flexDirection: "row",
         alignItems: "center",
+        marginBottom:54
     },
 
     image: {
--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {View, Text, Image} from "react-native";
-import Button from "../../../components/Button";
-import Pressable from "../../../components/Pressable";
 import AuthHeader from "../../../components/AuthHeader";
 import { styles } from "./styles";
+import Input from "../../../components/Input";
 
 const SignUp = () => {
     return (
         <View style={styles.container}>
             <AuthHeader title="Sign Up"/>
+            <Input label="Name" placeholder="John Doe"/>
+            <Input label="Email" placeholder="example@gmail.com"/>
+            <Input isPassword label="Password" placeholder="******"/>
         </View>
     )
 }
