5dbee4a364929c6778637625904f068ca5acfd2c
John Doe <johndoe@email.com>
2023-10-18 09:49:10 +0300

    Header

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
+import {View, Text} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./styles";
 
-import {View, Text} from 'react-native';
-
-import {SafeAreaView} from 'react-native-safe-area-context';
-
-import {styles} from './styles';
+import Header from "../../../components/Header";
 
 const Home = () => {
-  return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text>Home</Text>
-      </View>
-    </SafeAreaView>
-  );
-};
-
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Header showSearch={true} title="Find All You Need" />
+                <Text>Home</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
 export default Home;
