6eb43822968f5eb464171d87cf8d50317258d0eb
John Doe <johndoe@email.com>
2023-10-18 10:16:06 +0300

    Categories

--- src/screens/app/Favorites/styles.js
+++ src/screens/app/Favorites/styles.js
@@ -1,8 +1,9 @@
 import { StyleSheet } from "react-native"
+
 import { colors } from "../../../utils/colors"
 
 export const styles = StyleSheet.create( {
     container: {
-        textAlign: "center",
+        padding: 24,
     },
 });
