6a50cfe13a62df9f989956fdf224577a6da343a3
John Doe <johndoe@email.com>
2023-10-18 10:40:58 +0300

    Filtreerimine kategooria järgi

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {View, Text, FlatList} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
@@ -9,15 +9,30 @@ import { products } from "../../../data/products";
 import ProductHomeItem from "../../../components/ProductHomeItem";
 
 const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState()
+  const [selectedProducts, setSelectedProducts] = useState(products)
+
+  useEffect(() => {
+    if (selectedCategory) {
+      const updatedSelectedProducts = products.filter((product) => 
+          product?.category === selectedCategory);
+      setSelectedProducts(updatedSelectedProducts);
+  } else {
+      setSelectedProducts(products);
+  }
+}, [selectedCategory])
+
   const renderCategoryItem = ({item}) => {
-    console.log('item => ', item)
     return (
-      <CategoryBox title={item?.title} image={item?.image}/>
+      <CategoryBox onPress={() => setSelectedCategory(item?.id)}
+      isSelected={item.id === selectedCategory}
+      title={item?.title} 
+      image={item?.image}
+      />
       )
   }
 
   const renderProductItem = ({item}) => {
-    console.log('item => ', item);
     return (
         <ProductHomeItem {...item}/>
     )
@@ -28,7 +43,7 @@ const Home = () => {
             <View style={styles.container}>
                 <Header showSearch={true} title="Find All You Need" />
                 <FlatList showsHorizontalScrollIndicator={false} style={styles.list} horizontal data={categories} renderItem={renderCategoryItem} keyExtractor={(item, index) => String(index)} />
-                <FlatList numColumns={2} data={products} renderItem={renderProductItem} keyExtractor={(item) => String(item.id)} ListFooterComponent={<View style={{height: 250}}/>} />
+                <FlatList numColumns={2} data={selectedProducts} renderItem={renderProductItem} keyExtractor={(item) => String(item.id)} ListFooterComponent={<View style={{height: 250}}/>} />
             </View>
         </SafeAreaView>
     )
