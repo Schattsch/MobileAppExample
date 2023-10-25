8309bfbd7b49b7547718c2752000aadf87dfd572
John Doe <johndoe@email.com>
2023-10-25 09:05:38 +0300

    Piltide karusell

--- src/screens/app/ProductDetails/index.js
+++ src/screens/app/ProductDetails/index.js
@@ -3,6 +3,7 @@ import {ScrollView, Text, Image, View, Pressable} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 import Button from "../../../components/Button";
+import ImageCarusel from "../../../components/ImageCarusel/Index";
 
 const ProductDetails = ({navigation, route}) => {
     const {product} = route.params || {}
@@ -14,8 +15,12 @@ const ProductDetails = ({navigation, route}) => {
     return (
         <SafeAreaView style={styles.save}>
             <ScrollView>
+            {product?.images?.length ? (
+                <ImageCarusel images={product?.images} />
+                ) : (
                 <Image style={styles.image} source={{uri: product?.image}}/>
-                <View style={styles.content}>
+            )}
+            <View style={styles.content}>
                 <Text style={styles.title}>{product?.title}</Text>
                 <Text style={styles.price}>{product?.price}</Text>
                 <Text style={styles.description}>{product?.description}</Text>
